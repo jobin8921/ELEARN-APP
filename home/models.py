6124cb57b5d396da5ab798696dcb0335662161cb
JeswinDanJohnson <jeswinkoloor@gmail.com>
2025-03-01 00:17:08 +0530

    Added new fields in student model, updated views and also updated student_registeration template

--- home/models.py
+++ home/models.py
@@ -2,17 +2,20 @@ from django.db import models
 
 from django.contrib.auth.models import User
 
-# Model for Student
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_approved = models.BooleanField(default=False)
-    is_rejected = models.BooleanField(default=False)
-
 # Model for Adding Course
 class Course(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
-    # staff = models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True, blank=True)
+
+# Model for Student
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    is_approved = models.BooleanField(default=False)
+    is_rejected = models.BooleanField(default=False)
 
 # Model for Staff
 class Staff(models.Model):
