6cc5494b0398df2c1a870758fca3487b917d3754
Jobin Jose <127972673+jobin8921@users.noreply.github.com>
2025-05-07 20:15:46 +0530

    Update settings.py

--- elearning/settings.py
+++ elearning/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ze&p%j0qz8z+=gxe7zoe40a_duo#p-n4d%^#6c0jepsct43-=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:8000','web-production-08634.up.railway.app','127.0.0.1']
 
