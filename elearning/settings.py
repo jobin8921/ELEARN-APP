a7003423739207d04cab1dfe078d42ab15bec9f7
Jobin Jose <127972673+jobin8921@users.noreply.github.com>
2025-05-07 22:05:19 +0530

    Feature/student,staff login (#84)
    * course adding and putting on index page
    
    * teachers page setup
    
    * adding and deletion of event
    
    * flowting registration
    
    * button setting
    
    * staff/student registration setup
    
    * login setup as the floating window
    
    * added
    
    * added procfile
    
    * added the migartiosn file
    
    * updates
    
    * static files
    
    * chnages
    
    * addded
    
    * static
    
    * chnages
    
    * chnages
    
    * chnages
    
    * added
    
    * chnages
    
    * csrf
    
    * chnages
    
    * chnages
    
    * dd
    
    * add
    
    * added
    
    * databse
    
    ---------
    
    Co-authored-by: Eibel207 <eibelks60@gmail.com>

--- elearning/settings.py
+++ elearning/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,6 +87,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
