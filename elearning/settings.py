87bb3944e9a7ea40f492d598eadb593d78d51101
Jobin Jose <127972673+jobin8921@users.noreply.github.com>
2025-05-07 19:23:28 +0530

    Feature/student,staff login (#76)
    * course adding and putting on index page
    
    * teachers page setup
    
    * adding and deletion of event
    
    * flowting registration
    
    * button setting
    
    * staff/student registration setup
    
    * login setup as the floating window
    
    * added
    
    * added procfile
    
    * added the migartiosn file
    
    * updates
    
    * static files
    
    * chnages
    
    * addded
    
    * static
    
    * chnages
    
    * chnages
    
    * chnages
    
    ---------
    
    Co-authored-by: Eibel207 <eibelks60@gmail.com>

--- elearning/settings.py
+++ elearning/settings.py
@@ -128,12 +128,6 @@ STATICFILES_DIRS = [
 ]
 
 
-
-
-
-# Collect static files into this directory for production
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
