0f9a38db7f4013b806b91cd1b52e57ccf5412532
jobin8921 <jobinjose703@gmail.com>
2025-05-07 19:23:00 +0530

    chnages

--- elearning/settings.py
+++ elearning/settings.py
@@ -128,15 +128,6 @@ STATICFILES_DIRS = [
 ]
 
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 6b5939d2e808b2845ea26bbcbd1e331284b580c0
-# Collect static files into this directory for production
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
